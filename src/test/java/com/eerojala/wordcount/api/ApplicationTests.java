f0dd83904f60a389456c6830604571a0a516d922
eerojala <eero.ojala@helsinki.fi>
2023-11-10 18:48:56 +0200

    Initial commit

---
+++ src/test/java/com/eerojala/wordcount/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eerojala.wordcount.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
