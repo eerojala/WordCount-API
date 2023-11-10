e3f6a9dc4c8ac60964b344ba43897af86e9587df
eerojala <eero.ojala@helsinki.fi>
2023-11-10 23:19:04 +0200

    Refactored to accept form-data instead of JSON

--- src/main/java/com/eerojala/wordcount/api/service/WordCountService.java
+++ src/main/java/com/eerojala/wordcount/api/service/WordCountService.java
@@ -2,29 +2,43 @@ package com.eerojala.wordcount.api.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
 public class WordCountService {
-    public List<Map.Entry<String, Integer>> getMostCommonWords(String content, int k) {
-        var words = getWords(content);
-        var wordCountMap = Arrays.stream(words)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
+    public List<Map.Entry<String, Integer>> getMostCommonWords(MultipartFile file, int k) throws IOException {
+        String content = getContent(file);
+        var words = splitWords(content);
+        var wordCountMap = mapCountPerWord(words);
 
-        return wordCountMap.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .limit(k)
-                .toList();
+        return getMostCommonWords(wordCountMap, k);
     }
 
-    private String[] getWords(String content) {
+    private String getContent(MultipartFile file) throws IOException {
+        return new String(file.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    private String[] splitWords(String content) {
         return StringUtils.deleteAny(content, ",.'!?:;\"")
                 .toLowerCase()
                 .trim()
                 .replaceAll("\\s+", " ")
                 .split(" ");
+    }
+    private Map<String, Integer> mapCountPerWord(String[] words) {
+        return Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
+    }
+
+    private List<Map.Entry<String, Integer>> getMostCommonWords(Map<String, Integer> wordCountMap, int k) {
+        return wordCountMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(k)
+                .toList();
     }
 }
--- src/main/java/com/eerojala/wordcount/api/web/WordCountController.java
+++ src/main/java/com/eerojala/wordcount/api/web/WordCountController.java
@@ -6,8 +6,11 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +20,8 @@ public class WordCountController {
     private WordCountService service;
 
     @PostMapping("/wordcount")
-    public List<Map.Entry<String, Integer>> countWords(@Valid @RequestBody InputDto inputDto) {
-        return service.getMostCommonWords(inputDto.getContent(), inputDto.getAmount());
+    public List<Map.Entry<String, Integer>> countWords(@RequestParam("file") MultipartFile file,
+                                                       @RequestParam("amount") Integer amount) throws IOException {
+        return service.getMostCommonWords(file, amount);
     }
 }
